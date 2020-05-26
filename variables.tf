7722c531f02c30748cabd71de06cec9d54fa1551
vashish1 <vashishtiv@gmail.com>
2020-05-26 16:40:30 +0530

    config files for terraform added

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "gcloud-region"    { default = "europe-west2" }
+variable "gcloud-zone"      { default = "europe-west2-a" }
+variable "gcloud-project"   { default = "my-project1-267118" }
+variable "platform-name"    { default = "shippy-platform" }
